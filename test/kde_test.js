504de97a5c213f1f63cc9030fc684866f7ee0bfb
Daniel Levin <daniellevin2607@gmail.com>
2016-02-04 18:32:18 +0200

    Added Kernel Density Estimator

--- test/kde_test.js
+++ test/kde_test.js
@@ -1,4 +1,8 @@
 var distfit = require("../lib/distfit");
+var KDE = distfit.KDE;
 var chai = require("chai");
 var assert = chai.assert;
 var expect = chai.expect;
+
+describe("Kernel Density Estimation Test", function() {
+});
